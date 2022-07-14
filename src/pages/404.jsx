f8d14473269e7930a89f8ffd7ee7f759f0d7acc1
Alex Mant <al.mantione@gmail.com>
2022-07-14 10:44:31 +0200

    add 404error page

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/index.css"
+
+const Error404 = () => {
+  return (
+    <div className='error-container'>
+      <h1 className='error_title1'>404</h1>
+      <h3 className='error_title2'>PAGE NOT FOUND</h3>
+      <Link className='error_link' to="/sportsee/home/user/12">Revenir sur la dernière page visitée</Link>
+    </div>
+  );
+}
+
+export default Error404;
