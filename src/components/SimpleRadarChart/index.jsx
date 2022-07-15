b925eb395e6b69fccde4c8de7e53cd9fa22f3b29
Alex Mant <al.mantione@gmail.com>
2022-07-15 16:02:40 +0200

    add comment to radialChart.Jsx

--- src/components/SimpleRadarChart/index.jsx
+++ src/components/SimpleRadarChart/index.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useContext } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer, Layer } from 'recharts';
 import { DataContext } from '../../context/DataContext';
 import "./style.css"
 
-// const data = [
-//   {
-//     kind: 'Intensité',
-//     value: 120,
-//   }
-// ];
-
-
-
 export default function SimpleRadarChart () {
   const {data} = useContext(DataContext);
        
   const kind = data?.performance.kind;
+
+ /* A variable that is used to store the value of the `kind` property of the `data` object. */
   let arrKind;
+
+  /* A ternary operator. if kind Object value is undefined else transform kind object to array with only values of this object */
   kind === undefined ? <h1>Loading...</h1> : arrKind = Object.values(kind);
 
+  /**
+   * It takes a number and returns a string
+   * @returns The value of arrKind[iKind] if it exists, otherwise an empty string.
+   */
   const iKindToStrKind = (iKind) => {
     return arrKind[iKind] || '';
   }
 
   let dataPerformance = data?.performance.data;
+ /* A ternary operator. If dataPerformance is undefined, it returns undefined, otherwise it returns new value of kind object with iKindToStrKind function*/
   dataPerformance = dataPerformance?.map(({value, kind}) => {
-    return {value: value , kind: iKindToStrKind(kind-1)}
+    return {value , kind: iKindToStrKind(kind-1)}
   })
 
   return (
